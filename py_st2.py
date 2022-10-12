caf539ca97dcce3ebdbecc1cef8b0c4a2001ce16
hovhbag <104275087+hovhbag@users.noreply.github.com>
2022-10-13 02:01:48 +0400

    Update py_st2.py

--- py_st2.py
+++ py_st2.py
@@ -5,23 +5,23 @@ def dice():
 
 def second_play(n):
     while True:
-        l = [dice(), dice(), n]
-        sum = l[0] + l[1]
-        print(f"The sum of dice is {l[0]} + {l[1]} = {sum}")
+        l = {"dice1":dice(), "dice2":dice(), "goal":n}
+        sum = l["dice1"] + l["dice2"]
+        print(f"The sum of dice is {l['dice1']} + {l['dice2']} = {sum}")
         if sum == n:
             return "You won"
         elif sum == 7:
             return "You lose"
 
 def start():
-    l = [dice(), dice(), [2, 3, 12], [7, 11], [4, 5, 6, 8, 9, 10]]
-    sum = l[0] + l[1]
-    print(f"The sum of dice is {l[0]} + {l[1]} = {sum}")
-    if sum in l[2]:
+    l = {"dice1":dice(), "dice2":dice(), "casino":[2, 3, 12], "player":[7, 11], "other":[4, 5, 6, 8, 9, 10]}
+    sum = l['dice1'] + l['dice2']
+    print(f"The sum of dice is {l['dice1']} + {l['dice2']} = {sum}")
+    if sum in l['casino']:
         return "You lose"
-    elif sum in l[3]:
+    elif sum in l['player']:
         return "You won"
-    elif sum in l[4]:
+    elif sum in l['other']:
         print(f'Now your goal number is {sum}')
         return second_play(sum)
 
